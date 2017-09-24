3612fe2c8db203ab7a864d8f6819b22e866100c7
Joanna Jasnowska <joannajasnowska@desktop-at60cp9.connectis.local>
2017-09-24 12:37:22 +0200

    Create list-manager component

---
+++ src/app/list-manager/list-manager.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'todo-list-manager',
+  templateUrl: './list-manager.component.html',
+  styleUrls: ['./list-manager.component.css']
+})
+export class ListManagerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
