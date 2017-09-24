e65ff10719499d01f51c905ec6786a5893b05bdd
Joanna Jasnowska <joannajasnowska@desktop-at60cp9.connectis.local>
2017-09-24 12:41:47 +0200

    Move code from app component to list-manager component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,19 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
-
-  private todoList = [
-    {title: 'install NodeJS'},
-    {title: 'install Angular CLI'},
-    {title: 'create new app'},
-    {title: 'serve app'},
-    {title: 'develop app'},
-    {title: 'deploy app'},
-  ];
-
-  addItem(title: string): void {
-    console.log(title);
-    this.todoList.push({ title });
-  }
 }
--- src/app/list-manager/list-manager.component.ts
+++ src/app/list-manager/list-manager.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-manager.component.css']
 })
 export class ListManagerComponent implements OnInit {
+  title = 'todo';
+  private todoList = [
+    {title: 'install NodeJS'},
+    {title: 'install Angular CLI'},
+    {title: 'create new app'},
+    {title: 'serve app'},
+    {title: 'develop app'},
+    {title: 'deploy app'},
+  ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  addItem(title: string): void {
+    console.log(title);
+    this.todoList.push({ title });
   }
 
 }
